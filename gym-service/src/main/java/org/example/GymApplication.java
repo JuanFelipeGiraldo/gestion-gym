48c00947985f8a22ac31f64ae787a518eeac73ad
JuanFelipeGiraldo <juanfelipe98x@gmail.com>
2024-04-06 19:24:10 -0500

    test

--- gym-service/src/main/java/org/example/GymApplication.java
+++ gym-service/src/main/java/org/example/GymApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymApplication {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         SpringApplication.run(GymApplication.class, args);
         System.out.println("\nHello...World from GYM-SERVICIO");
+        System.out.println("hellooooo");
     }
 }
